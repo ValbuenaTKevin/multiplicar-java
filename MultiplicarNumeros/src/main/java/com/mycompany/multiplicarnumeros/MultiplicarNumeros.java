30967aea878425e6b0b6a2014313ec0663d9b299
Sin name <sinnombre@gmail.com>
2024-08-05 10:11:58 -0500

    Se elaboró la calculadora

--- MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
+++ MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.multiplicarnumeros;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -11,6 +13,11 @@ package com.mycompany.multiplicarnumeros;
 public class MultiplicarNumeros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner (System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int res = n1 * n2;
+        System.out.println(res);
+        scanner.close();
     }
 }
